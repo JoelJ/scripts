bdb1246915d8c4f7b2e2953a695191f5a05e5048
Joel Johnson <johnson.joel.86@gmail.com>
2012-07-24 10:22:57 -0600

    Looping

--- jenkins/UpdateJobsLive.groovy
+++ jenkins/UpdateJobsLive.groovy
@@ -2,12 +2,15 @@
 // If you manually change xml config files of Jenkins jobs,
 // this will force Jenkins to reload all the config from the disk to memory.
 // There might be a better way to do this... but I don't know how :)
-def projectName = 'Commit_master'
-def file = new File("/jenkins/jobs/"+projectName);
-def project = Items.load(Jenkins.getInstance(), file);
-def jenkins = Jenkins.getInstance();
-def field = Jenkins.class.getDeclaredField("items");
-field.setAccessible(true)
-def items = field.get(jenkins)
+def projectNames = ['Commit_omaster', 'Commit', 'Commit_rmaster']
+projectNames.each { projectName -> 
+  def file = new File("/jenkins/jobs/"+projectName);
+	def project = Items.load(Jenkins.getInstance(), file);
+	def jenkins = Jenkins.getInstance();
+	def field = Jenkins.class.getDeclaredField("items");
+	field.setAccessible(true)
+	def items = field.get(jenkins)
 
-items.put(project.name, project);
+	items.put(project.name, project);
+  	print "fixed $projectName\n"
+}
